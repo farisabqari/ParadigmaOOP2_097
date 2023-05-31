2a0ef7402e9306ac881017d198ad4686d0f8f665
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-05-31 18:40:40 +0800

    membuat class orang dengan public dan string nama

---
+++ multiwaris 2/multiwaris 2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "Orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "Orang dihapus\n" << endl;
+	}
+};
+
