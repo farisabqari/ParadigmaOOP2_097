4f9eaad8040b326b0ed4a512bad045f1ace89e82
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-05-31 18:26:15 +0800

    membuat class remotlampu dengan private dan public

---
+++ programEnkapsulasi/programEnkapsulasi.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class RemoteLampu {
+private:
+	string saklarNo[10];
+public:
+	void SetSaklarNo(int i, string value) {
+		saklarNo[i] = value;
+	}
+	string GetSaklarNo(int i) {
+		return saklarNo[i];
+	}
+};
+
